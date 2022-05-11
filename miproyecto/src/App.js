a2c8eb52b6b7b892c7e067705af405e5141bf851
EmiFunes91 <emifunes8@gmail.com>
2022-05-10 22:19:12 -0300

    navbar proyecto ecommerce

--- miproyecto/src/App.js
+++ miproyecto/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import ResponsiveAppBar from './components/Navbar/NavBar';
+
 
 function App() {
   return (
+    
     <div className="App">
-     <h1>Hola Mundo! Ecommerce2022</h1>
+      <ResponsiveAppBar/>
     </div>
+    
   );
 }
 
+
+
 export default App;
+
